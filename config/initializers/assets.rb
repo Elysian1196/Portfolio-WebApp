3d3949e9b73006121a0900c2b79f34b6cbdcd22f
Elysian1196 <mdowns2015@brandeis.edu>
2019-02-07 17:35:25 -0800

    removed variables from css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,7 +12,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-#Rails.application.config.assets.precompile += %w( _variables.scss )
-#Rails.application.config.assets.precompile += %w( resume.js )
-#Rails.application.config.assets.precompile += %w( resume.css )
-#Rails.application.config.assets.precompile += %w( jquery.easing.js )
+Rails.application.config.assets.precompile += %w( resume.js )
+Rails.application.config.assets.precompile += %w( resume.css )
+Rails.application.config.assets.precompile += %w( jquery.easing.js )
