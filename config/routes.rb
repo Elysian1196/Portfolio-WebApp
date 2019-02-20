78c2f6ae03bfa7d5eb540d082e166b0556a45332
Elysian1196 <mdowns2015@brandeis.edu>
2019-02-20 15:58:37 -0800

    added personal info

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'portfolio/home'
+  #get 'portfolio/home'
+  get '/home', to: 'portfolio#home'
+  get '/projects', to: 'portfolio#project'
   get 'portfolio/project'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'portfolio#home'
